1d27bf12aef5426a8434e64fca983211214cc7da
erichanzl <erichanzl@yahoo.com>
2015-12-05 17:58:56 -0500

    Hour 4 corrections

---
+++ teach/Java24/src/com/java24hours/Square.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+class Square {
+   public static void main(String[] arguments) {
+      int x = 4;
+      int y = 5;
+      int sum = (x*x) + (y*y);
+      System.out.println("The result of " + x + " squared plus "
+          + y + " squared equals " + sum);
+   }
+}
